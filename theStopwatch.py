84dfc7dba37b2168d8021fceaf2cbeddd5b84ac8
Mixel-MurP-427 <connor.mullarky@outlook.com>
2025-09-25 15:30:16 -0700

    Collected data and saved final to savaData1.json

--- theStopwatch.py
+++ theStopwatch.py
@@ -1,17 +1,17 @@
-#Note that this stopwatch is tailored to my specific needs (rounding, 5 iteration sections, speed_factor),
+#Note that this stopwatch is tailored to my specific needs (rounding, 6 iteration sections, speed_factor),
 #   so will need some tweaking when used for other purposes
 import json
 from time import perf_counter_ns #returns time in nanoseconds
 
-speed_factor = 1 #set to 1 for normal functionality
+speed_factor = 0.5 #set to 1 for normal functionality
 
 output = {}
-for section in range(1,6):
+for section in range(4,6):
     output[section] = []
 
     print(f"\nBegin section {section}")
 
-    for _ in range(5):
+    for _ in range(6):
         input("hit Enter to start")
         start_time = perf_counter_ns()
         input("hit Enter to stop")
@@ -21,8 +21,7 @@ for section in range(1,6):
         print(f"time = {the_time} s")
         output[section].append(the_time)
 
-print()
-print(output)
-with open("savaData.json", "w") as myFile:
-    json.dump(output, myFile)
+    with open("savaData.json", "w") as myFile:
+        json.dump(output, myFile)
+
 print("end program!")
