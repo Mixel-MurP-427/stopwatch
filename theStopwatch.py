60f8e464e3ef2c9dd835341d5ba8565e64bbd2b9
Mixel-MurP-427 <connor.mullarky@outlook.com>
2025-09-25 14:26:23 -0700

    Created working prototype. Ought to need no debugs

---
+++ theStopwatch.py
@@ -0,0 +1,28 @@
+#Note that this stopwatch is tailored to my specific needs (rounding, 5 iteration sections, speed_factor),
+#   so will need some tweaking when used for other purposes
+import json
+from time import perf_counter_ns #returns time in nanoseconds
+
+speed_factor = 1 #set to 1 for normal functionality
+
+output = {}
+for section in range(1,6):
+    output[section] = []
+
+    print(f"\nBegin section {section}")
+
+    for _ in range(5):
+        input("hit Enter to start")
+        start_time = perf_counter_ns()
+        input("hit Enter to stop")
+        stop_time = perf_counter_ns()
+
+        the_time = round((stop_time - start_time) / 1000000000 * speed_factor, 3)
+        print(f"time = {the_time} s")
+        output[section].append(the_time)
+
+print()
+print(output)
+with open("savaData.json", "w") as myFile:
+    json.dump(output, myFile)
+print("end program!")
